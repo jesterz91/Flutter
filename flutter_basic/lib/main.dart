edb30379009e444a8dfcb8978a4c8eceddfaff42
jesterz91 <jesterz91@naver.com>
2019-07-26 00:32:43 +0900

    StatefulWidget

--- flutter_basic/lib/main.dart
+++ flutter_basic/lib/main.dart
@@ -3,22 +3,52 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter'),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-        body: Text(
-          '안녕 플러터',
-          style: TextStyle(fontSize: 30),
-        )),
-    );
+        home: HelloPage("Hello Flutter"));
+  }
+}
+
+class HelloPage extends StatefulWidget {
+  final String title;
+
+  HelloPage(this.title);
+
+  @override
+  State createState() => _HelloPageSate();
+}
+
+class _HelloPageSate extends State<HelloPage> {
+  String _message = 'Hello Flutter';
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add), onPressed: () => _changeMessage()),
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(_message, style: TextStyle(fontSize: 30)),
+            Text('$_counter', style: TextStyle(fontSize: 30)),
+          ],
+        )));
+  }
+
+  void _changeMessage() {
+    setState(() {
+      _message = "안녕 플러터";
+      _counter++;
+    });
   }
 }
