f5f2ba33b9f2bb0ff9d8a24f51563ddb1ccc48cc
jesterz91 <jesterz91@naver.com>
2019-08-24 23:01:27 +0900

    함수형 프로그래밍

---
+++ dart_basic/scream.dart
@@ -0,0 +1,11 @@
+tring scream(int length) => "A${'a' * length}h!";
+
+main() {
+  final values = [1, 2, 3, 5, 10, 50];
+  // 명령형 스타일
+  for (var length in values) {
+    print(scream(length));
+  }
+  // 함수형 스타일
+  values.skip(1).take(3).map(scream).forEach(print);
+}
