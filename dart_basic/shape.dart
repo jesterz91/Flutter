9860bb8dff37bd2386b00efd8447ad06d550005a
jesterz91 <jesterz91@naver.com>
2019-08-24 22:46:11 +0900

    팩토리 구현

---
+++ dart_basic/shape.dart
@@ -0,0 +1,41 @@
+import 'dart:math';
+
+abstract class Shape {
+  // factory 생성자
+  factory Shape(String type) {
+    if (type == 'circle') return Circle(2);
+    if (type == 'square') return Square(2);
+    throw 'Can\'t create $type.';
+  }
+  num get area;
+}
+
+// top-level 함수
+Shape shapeFactory(String type) {
+  if (type == 'circle') return Circle(2);
+  if (type == 'square') return Square(2);
+  throw 'Can\'t create $type.';
+}
+
+class Circle implements Shape {
+  final num radius;
+  Circle(this.radius);
+  num get area => pi * pow(radius, 2);
+}
+
+class Square implements Shape {
+  final num side;
+  Square(this.side);
+  num get area => pow(side, 2);
+}
+
+main() {
+  //final circle = Circle(2);
+  //final square = Square(2);
+  
+  final circle = shapeFactory('circle');
+  final square = Shape('square');  
+  
+  print(circle.area); // 12.566370614359172
+  print(square.area); // 4
+}
