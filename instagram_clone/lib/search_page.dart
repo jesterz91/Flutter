b2edd6f87bd22ed0544795f1039abfe3d8c89cc3
jesterz91 <jesterz91@naver.com>
2019-09-05 13:49:17 +0900

    인스타그램 클론 : _SearchPage UI 작성

---
+++ instagram_clone/lib/search_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildGridView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        backgroundColor: Colors.blue,
+        child: Icon(Icons.create),
+      ),
+    );
+  }
+
+  GridView _buildGridView() {
+    return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          crossAxisSpacing: 1.0,
+          mainAxisSpacing: 1.0,
+          childAspectRatio: 1.0,
+        ),
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return _buildListItem(context, index);
+        });
+  }
+  
+  Widget _buildListItem(BuildContext context, int index) {
+    return Image.network("https://avatars1.githubusercontent.com/u/66577?s=460&v=4", fit: BoxFit.cover,);
+  }
+}
