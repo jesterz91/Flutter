4da7cb570bc9855b562436ee12adc96f808d1a3b
jesterz91 <jesterz91@naver.com>
2019-09-05 13:36:34 +0900

    인스타그램 클론 : _AccountPage UI 작성

---
+++ instagram_clone/lib/account_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class AccountPage extends StatefulWidget {
+  @override
+  _AccountPageState createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildPadding(),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: Text('Account'),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.exit_to_app),
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+
+  Padding _buildPadding() {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  SizedBox(
+                    width: 80.0,
+                    height: 80.0,
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          "https://avatars3.githubusercontent.com/u/25954406?s=460&v=4"),
+                    ),
+                  ),
+                  Container(
+                    width: 80.0,
+                    height: 80.0,
+                    alignment: Alignment.bottomRight,
+                    child: SizedBox(
+                        width: 25.0,
+                        height: 25.0,
+                        child: FloatingActionButton(
+                          onPressed: () {},
+                          backgroundColor: Colors.blue,
+                          child: Icon(Icons.add),
+                        )),
+                  )
+                ],
+              ),
+              Padding(padding: EdgeInsets.all(8.0)),
+              Text(
+                "이름",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+              )
+            ],
+          ),
+          Text(
+            '0\n게시물',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18.0),
+          ),
+          Text(
+            '0\n팔로워',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18.0),
+          ),
+          Text(
+            '0\n팔로잉',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18.0),
+          ),
+        ],
+      ),
+    );
+  }
+}
