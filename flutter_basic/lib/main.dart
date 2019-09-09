f98b9afdfc2d934d206926a1e993b7f7d027bc03
jesterz91 <jesterz91@naver.com>
2019-09-09 20:43:44 +0900

    RxDart, BehaviorSubject

--- flutter_basic/lib/main.dart
+++ flutter_basic/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/listview_page.dart';
+import 'package:rxdart/rxdart.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,56 +8,132 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: HelloPage("Hello Flutter"));
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Hello Flutter'),
+    );
   }
 }
 
-class HelloPage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
   final String title;
 
-  HelloPage(this.title);
-
   @override
-  State createState() => _HelloPageSate();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _HelloPageSate extends State<HelloPage> {
-  String _message = 'Hello Flutter';
+class _MyHomePageState extends State<MyHomePage> {
+
+  final _counterSubject = BehaviorSubject<int>();
   int _counter = 0;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _counterSubject.close();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add), onPressed: () => _changeMessage()),
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(_message, style: TextStyle(fontSize: 30)),
-                Text('$_counter', style: TextStyle(fontSize: 30)),
-                RaisedButton(
-                  child: Text('화면 이동'),
-                  onPressed: () => {
-                    MaterialPageRoute(builder: (context) {
-                      
-                    })
-                  })
-              ],
-            )));
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.settings), onPressed: () => print("setting")),
+        ],
+      ),
+      drawer: _buildDrawer(context),
+      body: _buildBody(context),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _counterSubject.add(++_counter),
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
   }
 
-  void _changeMessage() {
-    setState(() {
-      _message = "안녕 플러터";
-      _counter++;
-    });
+  Widget _buildBody(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('버튼 클릭 수',),
+          StreamBuilder<int>(
+            stream: _counterSubject.stream,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(
+                  '${snapshot.data}',
+                  style: Theme.of(context).textTheme.display1,
+                );
+              }
+              return Text('$_counter', style: Theme.of(context).textTheme.display1,);
+            }
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  width: 75,
+                  height: 75,
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://avatars2.githubusercontent.com/u/24826856?s=180&v=4"),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(4)),
+                Text("Nick Name", style: TextStyle(color: Colors.white),),
+                Padding(padding: EdgeInsets.all(4)),
+                Text("E-mail", style: TextStyle(color: Colors.white),),
+              ],
+            ),
+            decoration: BoxDecoration(color: Colors.blue),
+          ),
+          ListTile(
+            title: Text("리스트"),
+            leading: Icon(
+              Icons.list,
+            ),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (BuildContext context) => ListViewPage()));
+            },
+          ),
+          ListTile(
+            title: Text("메뉴2"),
+            leading: Icon(
+              Icons.star,
+            ),
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          ListTile(
+            title: Text("메뉴3"),
+            leading: Icon(Icons.search),
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
