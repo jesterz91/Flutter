31afe291b540326ede8416cfdb7e27e4614b4ed5
jesterz91 <jesterz91@naver.com>
2019-09-19 11:28:53 +0900

    플랫폼별 테마 정의

---
+++ friendlychat/lib/chat_message.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:friendlychat/main.dart';
+
+class ChatMessage extends StatelessWidget {
+
+  final String text;
+  final AnimationController animationController;
+
+  ChatMessage({this.text, this.animationController});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizeTransition(
+      sizeFactor: CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      axisAlignment: 0.0,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(child: Text(name[0])),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(name, style: Theme.of(context).textTheme.subhead),
+                  Container(
+                    margin: EdgeInsets.only(top: 5.0),
+                    child: Text(text),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- friendlychat/lib/main.dart
+++ friendlychat/lib/main.dart
@@ -1,12 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:friendlychat/chat_message.dart';
 
 void main() => runApp(FriendlychatApp());
 
+const String name = "Dev Lee";
+
 class FriendlychatApp extends StatelessWidget {
+
+  final ThemeData kIOSTheme = ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.grey[100],
+    primaryColorBrightness: Brightness.light,
+  );
+
+  final ThemeData kDefaultTheme = ThemeData(
+    primarySwatch: Colors.purple,
+    accentColor: Colors.orangeAccent[400],
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
       home: ChatScreen(),
     );
   }
@@ -17,14 +37,41 @@ class ChatScreen extends StatefulWidget {
   State createState() => ChatScreenState();
 }
 
-class ChatScreenState extends State<ChatScreen> {
-  final _textController = new TextEditingController();
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final _textController = TextEditingController();
+  bool _isComposing = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Friendlychat")),
-      body: _buildTextComposer(),
+      appBar: AppBar(
+        title: Text("Friendlychat"),
+        elevation:
+        Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+                child: ListView.builder(
+                  padding: EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemBuilder: (context, index) => _messages[index],
+                  itemCount: _messages.length,
+                )
+            ),
+            Divider(height: 1.0,),
+            Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
+            )
+          ],
+        ),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+            ? BoxDecoration(border: Border(top: BorderSide(color: Colors.grey[200])))
+            : null,
+      ),
     );
   }
 
@@ -32,29 +79,62 @@ class ChatScreenState extends State<ChatScreen> {
     return IconTheme(
         data: IconThemeData(color: Theme.of(context).accentColor),
         child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          margin: EdgeInsets.symmetric(horizontal: 8.0),
           child: Row(
             children: <Widget>[
               Flexible(
                 child: TextField(
                   controller: _textController,
+                  onChanged: (String text) {
+                    setState(() => _isComposing = text.length > 0);
+                  },
                   onSubmitted: _handleSubmitted,
                   decoration: InputDecoration.collapsed(hintText: "메시지를 입력하세요"),
                 ),
               ),
               Container(
                 margin: EdgeInsets.symmetric(horizontal: 4.0),
-                child: IconButton(
-                    icon: Icon(Icons.send),
-                    onPressed: () => _handleSubmitted(_textController.text)),
-              ),
+                child: Theme.of(context).platform == TargetPlatform.iOS
+                  ? CupertinoButton(
+                      child: Text("전송"),
+                      onPressed: _isComposing
+                        ? () => _handleSubmitted(_textController.text)
+                        : null
+                    )
+                  : IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: _isComposing
+                        ? () => _handleSubmitted(_textController.text)
+                        : null
+                    ),
+              )
             ],
           ),
         )
     );
   }
 
-  void _handleSubmitted(String text) {
+  void _handleSubmitted(String message) {
     _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
+    ChatMessage chatMessage = ChatMessage(
+      text: message,
+      animationController: AnimationController(
+        duration: Duration(milliseconds: 700),
+        vsync: this,
+      ),
+    );
+    setState(() => _messages.insert(0, chatMessage));
+    chatMessage.animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    for (ChatMessage message in _messages) {
+      message.animationController.dispose();
+    }
+    super.dispose();
   }
 }
